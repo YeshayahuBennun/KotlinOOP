245adb872a0f84b90fb40f6e0995df69f9029ba7
YeshayahuBennun <ybennun@yahoo.com>
2021-09-22 01:20:17 +0300

    commit#3 - Constructors KotlinOOP

--- app/src/main/java/com/ybennun/kotlinoop/Car.kt
+++ app/src/main/java/com/ybennun/kotlinoop/Car.kt
@@ -1,9 +1,16 @@
 package com.ybennun.kotlinoop
 
-class Car {
-    val speed: Int = 70
-    val gear: Int = 5
-    val seat: Int = 4
+class Car(
+     val speed: Int,
+     val gear: Int,
+     val seat: Int
+) {
+
+    var distance: Int = 0
+
+    constructor(time: Int, speed: Int, gear: Int, seat: Int) : this(speed, gear, seat) {
+        distance = speed * time
+    }
 
     fun speedUp() = speed * 5
 }
--- app/src/main/java/com/ybennun/kotlinoop/MyMain.kt
+++ app/src/main/java/com/ybennun/kotlinoop/MyMain.kt
@@ -1,9 +1,13 @@
 package com.ybennun.kotlinoop
 
 fun main() {
-    val car1 = Car()
+    val car1 = Car(50, 4, 2)
 
     println("car1 speed is: ${car1.speed}, gear is: ${car1.gear}, seat is: ${car1.seat}")
 
     println("car1 speed after speeding up is: ${car1.speedUp()}")
+
+    var car2 = Car(5, 60, 5, 4)
+
+    println("The distance cross by speed ${car2.speed} will be ${car2.distance}")
 }
