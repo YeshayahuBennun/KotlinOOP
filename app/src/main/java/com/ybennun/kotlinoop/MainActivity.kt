e7e838f1e1f00bbaf9f6d67c39e31bda1667d7a9
YeshayahuBennun <ybennun@yahoo.com>
2021-09-22 16:37:24 +0300

    commit#6 -  Exception Handling(try, catch) KotlinOOP

--- app/src/main/java/com/ybennun/kotlinoop/MainActivity.kt
+++ app/src/main/java/com/ybennun/kotlinoop/MainActivity.kt
@@ -2,10 +2,17 @@ package com.ybennun.kotlinoop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            20 / 0
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
     }
 }
